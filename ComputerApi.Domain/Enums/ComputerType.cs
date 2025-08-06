1731b7940a0214680ecee6025c2e8d215bbcf950
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-05 22:38:48 -0600

    Add ComputerType enum to Domain.Enums namespace

---
+++ ComputerApi.Domain/Enums/ComputerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerApi.Domain.Enums
+{
+    public enum ComputerType
+    {
+        Laptop = 0,
+        Desktop = 1
+    }
+}
