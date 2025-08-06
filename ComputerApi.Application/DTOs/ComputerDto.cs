e5b8b4bcb78265fcae70399b1f57627d694eb24c
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-05 22:37:27 -0600

    Add ComputerDto classes for computer management

---
+++ ComputerApi.Application/DTOs/ComputerDto.cs
@@ -0,0 +1,27 @@
+﻿using ComputerApi.Domain.Enums;
+
+namespace ComputerApi.Application.DTOs
+{
+    public class ComputerDto
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; } = string.Empty;
+        public ComputerType Type { get; set; }
+        public int ManufacturingYear { get; set; }
+        public List<SoftwareDto> InstalledSoftwares { get; set; } = new();
+    }
+
+    public class CreateComputerDto
+    {
+        public string Brand { get; set; } = string.Empty;
+        public ComputerType Type { get; set; }
+        public int ManufacturingYear { get; set; }
+    }
+
+    public class UpdateComputerDto
+    {
+        public string Brand { get; set; } = string.Empty;
+        public ComputerType Type { get; set; }
+        public int ManufacturingYear { get; set; }
+    }
+}
