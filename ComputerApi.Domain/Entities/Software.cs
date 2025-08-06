bd8b3d5ace6ef666117121a6c58e2fe024263bb3
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-05 22:38:08 -0600

    Add Software class with validation and relationships

---
+++ ComputerApi.Domain/Entities/Software.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputerApi.Domain.Entities
+{
+    public class Software
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string Version { get; set; } = string.Empty;
+
+        public virtual ICollection<InstalledSoftware> InstalledSoftwares { get; set; } = new List<InstalledSoftware>();
+    }
+}
