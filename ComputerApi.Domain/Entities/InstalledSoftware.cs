2b7f642912ddb131945c16bbf09ee648f216b4d3
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-05 22:37:57 -0600

    Add InstalledSoftware class to domain entities

---
+++ ComputerApi.Domain/Entities/InstalledSoftware.cs
@@ -0,0 +1,12 @@
+﻿namespace ComputerApi.Domain.Entities
+{
+    public class InstalledSoftware
+    {
+        public int ComputerId { get; set; }
+        public int SoftwareId { get; set; }
+        public DateTime InstallationDate { get; set; }
+
+        public virtual Computer Computer { get; set; } = null!;
+        public virtual Software Software { get; set; } = null!;
+    }
+}
