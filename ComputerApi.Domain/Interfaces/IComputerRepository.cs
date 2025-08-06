b0d4e5a017b7883936179196ae61651ee2a804bc
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-05 22:40:58 -0600

    Add IComputerRepository interface for computer management

---
+++ ComputerApi.Domain/Interfaces/IComputerRepository.cs
@@ -0,0 +1,15 @@
+﻿using ComputerApi.Domain.Entities;
+
+namespace ComputerApi.Domain.Interfaces
+{
+    public interface IComputerRepository
+    {
+        Task<Computer?> GetByIdAsync(int id);
+        Task<Computer?> GetByIdWithSoftwareAsync(int id);
+        Task<IEnumerable<Computer>> GetAllAsync();
+        Task<Computer> CreateAsync(Computer computer);
+        Task<Computer> UpdateAsync(Computer computer);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
