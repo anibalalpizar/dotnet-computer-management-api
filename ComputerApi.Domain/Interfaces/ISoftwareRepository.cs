45f5e17f1a3e9709ca0c0970a396179cb00ac9d5
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-05 22:40:44 -0600

    Add ISoftwareRepository interface for software management

---
+++ ComputerApi.Domain/Interfaces/ISoftwareRepository.cs
@@ -0,0 +1,18 @@
+﻿using ComputerApi.Domain.Entities;
+
+namespace ComputerApi.Domain.Interfaces
+{
+    public interface ISoftwareRepository
+    {
+        Task<Software?> GetByIdAsync(int id);
+        Task<IEnumerable<Software>> GetAllAsync();
+        Task<Software> CreateAsync(Software software);
+        Task<Software> UpdateAsync(Software software);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+        Task<IEnumerable<Software>> GetSoftwareByComputerIdAsync(int computerId);
+        Task<bool> AddSoftwareToComputerAsync(int computerId, int softwareId);
+        Task<bool> RemoveSoftwareFromComputerAsync(int computerId, int softwareId);
+        Task<bool> IsSoftwareInstalledOnComputerAsync(int computerId, int softwareId);
+    }
+}
