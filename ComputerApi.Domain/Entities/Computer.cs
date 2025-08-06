e8393ae23b4be0cd7bdc27478fcc47e7640f0d42
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-05 22:37:45 -0600

    Add Computer class with validation and properties

---
+++ ComputerApi.Domain/Entities/Computer.cs
@@ -0,0 +1,21 @@
+﻿using ComputerApi.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ComputerApi.Domain.Entities
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Brand { get; set; } = string.Empty;
+
+        public ComputerType Type { get; set; }
+
+        [Range(1990, 2030)]
+        public int ManufacturingYear { get; set; }
+
+        public virtual ICollection<InstalledSoftware> InstalledSoftwares { get; set; } = new List<InstalledSoftware>();
+    }
+}
