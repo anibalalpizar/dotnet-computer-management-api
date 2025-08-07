2cf1281a6e20dc008e547caf77d9edde9976ed2e
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-06 21:29:01 -0600

    Add AutoMapper mappings for Computer and Software DTOs

---
+++ ComputerApi.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using ComputerApi.Application.DTOs;
+using ComputerApi.Domain.Entities;
+
+namespace ComputerApi.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Computer mappings
+            CreateMap<Computer, ComputerDto>()
+                .ForMember(dest => dest.InstalledSoftwares, opt => opt.MapFrom(src =>
+                    src.InstalledSoftwares.Select(ins => new SoftwareDto
+                    {
+                        Id = ins.Software.Id,
+                        Description = ins.Software.Description,
+                        Version = ins.Software.Version,
+                        InstallationDate = ins.InstallationDate
+                    })));
+
+            CreateMap<CreateComputerDto, Computer>();
+            CreateMap<UpdateComputerDto, Computer>();
+
+            // Software mappings
+            CreateMap<Software, SoftwareDto>();
+            CreateMap<CreateSoftwareDto, Software>();
+            CreateMap<UpdateSoftwareDto, Software>();
+        }
+    }
+}
