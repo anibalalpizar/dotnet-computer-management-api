134d446953236feffc417182254bc812cd00cff9
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-06 21:28:11 -0600

    Add IUnitOfWork interface for transaction management

---
+++ ComputerApi.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace ComputerApi.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IComputerRepository Computers { get; }
+        ISoftwareRepository Software { get; }
+        Task<int> SaveChangesAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
