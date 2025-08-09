6141e2e3da5cea015459ea31b4b6b19d294c5e05
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-08 21:31:52 -0600

    Add ISoftwareService interface for software management

---
+++ ComputerApi.Application/Services/ISoftwareService.cs
@@ -0,0 +1,19 @@
+﻿using ComputerApi.Application.Common;
+using ComputerApi.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerApi.Application.Services
+{
+    public interface ISoftwareService
+    {
+        Task<Result<IEnumerable<SoftwareDto>>> GetAllSoftwareAsync();
+        Task<Result<SoftwareDto>> GetSoftwareByIdAsync(int id);
+        Task<Result<SoftwareDto>> CreateSoftwareAsync(CreateSoftwareDto softwareDto);
+        Task<Result<SoftwareDto>> UpdateSoftwareAsync(int id, UpdateSoftwareDto softwareDto);
+        Task<Result> DeleteSoftwareAsync(int id);
+    }
+}
