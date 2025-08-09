ca689aa95aa809bd067b7c8c596f8f0b7a4257f5
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-08 21:32:08 -0600

    Add IComputerService interface for computer management

---
+++ ComputerApi.Application/Services/IComputerService.cs
@@ -0,0 +1,22 @@
+﻿using ComputerApi.Application.Common;
+using ComputerApi.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerApi.Application.Services
+{
+    public interface IComputerService
+    {
+        Task<Result<IEnumerable<ComputerDto>>> GetAllComputersAsync();
+        Task<Result<ComputerDto>> GetComputerByIdAsync(int id);
+        Task<Result<ComputerDto>> CreateComputerAsync(CreateComputerDto computerDto);
+        Task<Result<ComputerDto>> UpdateComputerAsync(int id, UpdateComputerDto computerDto);
+        Task<Result> DeleteComputerAsync(int id);
+        Task<Result<IEnumerable<SoftwareDto>>> GetComputerSoftwareAsync(int computerId);
+        Task<Result> AddSoftwareToComputerAsync(int computerId, int softwareId);
+        Task<Result> RemoveSoftwareFromComputerAsync(int computerId, int softwareId);
+    }
+}
