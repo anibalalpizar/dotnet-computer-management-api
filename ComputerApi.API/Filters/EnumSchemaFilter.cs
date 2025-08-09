689ad587ccc78d2156e9ed8388c76ec2acb4b22c
anibalalpizar <anibal.alpizar14@gmail.com>
2025-08-08 21:32:35 -0600

    Add EnumSchemaFilter for OpenAPI enum documentation

---
+++ ComputerApi.API/Filters/EnumSchemaFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.ComponentModel;
+
+namespace ComputerApi.API.Middleware
+{
+    public class EnumSchemaFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (context.Type.IsEnum)
+            {
+                schema.Type = "string";
+                schema.Enum.Clear();
+
+                foreach (var enumName in Enum.GetNames(context.Type))
+                {
+                    var memberInfo = context.Type.GetField(enumName);
+                    var descriptionAttribute = memberInfo?.GetCustomAttributes(typeof(DescriptionAttribute), false)
+                        .Cast<DescriptionAttribute>()
+                        .FirstOrDefault();
+
+                    var description = descriptionAttribute?.Description ?? enumName;
+                    schema.Enum.Add(new Microsoft.OpenApi.Any.OpenApiString(enumName));
+                }
+            }
+        }
+    }
+}
